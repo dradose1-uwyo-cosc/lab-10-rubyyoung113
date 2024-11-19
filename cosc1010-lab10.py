f8f706127dafea2206b01e5f4caff98289c99cd1
rubyyoung113 <ryoung19@uwyo.edu>
2024-11-19 03:07:36 +0000

    done

--- cosc1010-lab10.py
+++ cosc1010-lab10.py
@@ -1,45 +1,54 @@
-# Your Name Here
+# Ruby Young 
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section: 
-# Sources, people worked with, help given to: 
-# your
-# comments
-# here
+# 27th November 
+# Lab 10
+# Lab Section: 16
+# Sources, people worked with, help given to: Serina Abriola, Eugenia Ceinos, Ryan Zafft 
 
-#import modules you will need 
 
-from hashlib import sha256 
+# Files and Exceptions
+# For this assignment, you will be writing a program to "crack" a password. You will need to open the file `hash` as this is the password you are trying to "crack."
+# To begin, you will need to open the 'rockyou.txt' file:
+# - This file contains a list of compromised passwords from the rockyou dump.
+# - This is an abridged version, as the full version is quite large.
+# - The file contains the plaintext version of the passwords. You will need to hash them to check against the password hash you are trying to crack.
+# - You can use the provided `get_hash()` function to generate the hashes.
+# - Be careful, as "hello" and "hello " would generate a different hash.
+# You will need to include a try-except-catch block in your code.
+# - The reading of files needs to occur in the try blocks.
+# - Read in the value stored within `hash`.
+# - You must use a try and except block.
+# Read in the passwords in `rockyou.txt`.
+# - Again, you need a try-except-else block.
+# Hash each individual password and compare it against the stored hash.
+# - When you find the match, print the plaintext version of the password.
+
+
 from pathlib import Path
+from hashlib import sha256 
 
 def get_hash(to_hash):
     """You can use """
     return sha256(to_hash.encode('utf-8')).hexdigest().upper()
 
 
+try: 
+    rockyoufile = Path("rockyou.txt")
+except:
+    print(f"The file rockyou.txt is unable to be read") 
+else: 
+    filecontents = rockyoufile.read_text()
+    passwords = filecontents.splitlines()
 
-# Files and Exceptions
-
-# For this assignment, you will be writing a program to "crack" a password. You will need to open the file `hash` as this is the password you are trying to "crack."
-
-# To begin, you will need to open the 'rockyou.txt' file:
-# - This file contains a list of compromised passwords from the rockyou dump.
-# - This is an abridged version, as the full version is quite large.
-# - The file contains the plaintext version of the passwords. You will need to hash them to check against the password hash you are trying to crack.
-#   - You can use the provided `get_hash()` function to generate the hashes.
-#   - Be careful, as "hello" and "hello " would generate a different hash.
-
-# You will need to include a try-except-catch block in your code.
-# - The reading of files needs to occur in the try blocks.
+try: 
+    hashfile = Path("hash")
+    hash = hashfile.read_text()
+except:
+    print(f"The file hash is unable to be read")
+else: 
+    for password in passwords: 
+        if get_hash(password) == hash: 
+            print(password)
+ 
 
 
-# - Read in the value stored within `hash`.
-#   - You must use a try and except block.
-
-
-# Read in the passwords in `rockyou.txt`.
-# - Again, you need a try-except-else block.
-# Hash each individual password and compare it against the stored hash.
-# - When you find the match, print the plaintext version of the password.
-# - End your loop.
